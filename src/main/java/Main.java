e4aa5a13ec08b0c8f97153e3533bcfbe4bb2bad2
Vignesh <720co.main@gmail.com>
2024-07-07 13:57:58 +0530

    Pass 1st Stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,9 +18,20 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
+       InputStream inputStream = clientSocket.getInputStream();
+       BufferedReader reader =
+           new BufferedReader(new InputStreamReader(inputStream));
+       String line = reader.readLine();
+       System.out.println(line);
+       String[] httpPath = line.split(" ", 0);
+       System.out.println(httpPath[1]);
+       OutputStream output = clientSocket.getOutputStream();
+       if (httpPath[1].equals("/")) {
+         output.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
+       } else {
+         output.write(("HTTP/1.1 404 Not Found\r\n\r\n").getBytes());
+       }
        System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write(
-          "HTTP/1.1 200 OK\r\n\r\n".getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
