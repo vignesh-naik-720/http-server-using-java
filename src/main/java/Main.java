70383544f41aec76bd235ed0a72664e75556e2eb
Vignesh <720co.main@gmail.com>
2024-07-07 13:43:14 +0530

    Pass 1st Stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,8 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       clientSocket.getOutputStream().write(
+          "HTTP/1.1 200 OK\r\n\r\n".getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
